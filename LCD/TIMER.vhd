b9e365ca6c2dca6d76d8d1065e8b81843599800b
zen451019 <dmanuele74@gmail.com>
2026-01-02 10:39:23 -0500

    Added: safety feature to prevent timer errors

--- LCD/TIMER.vhd
+++ LCD/TIMER.vhd
@@ -13,24 +13,32 @@ END ENTITY;
 
 ARCHITECTURE COMP OF TIMER IS
     SIGNAL COUNT : INTEGER RANGE 0 TO 100000 := 0;
+    SIGNAL PREV_FAST_MODE : STD_LOGIC := '0';
+
     CONSTANT MAX_SLOW : INTEGER := 100000; -- 2ms
     CONSTANT MAX_FAST : INTEGER := 2500; -- 50us
-    SIGNAL MAX_COUNT : INTEGER;
+    SIGNAL MAX_COUNT : INTEGER := MAX_SLOW;
 BEGIN
-
-    MAX_COUNT <= MAX_FAST WHEN FAST_MODE = '1' ELSE
-        MAX_SLOW;
 
     PROCESS (CLK, RST)
     BEGIN
         IF RST = '1' THEN
             COUNT <= 0;
             ENABLE <= '0';
+            MAX_COUNT <= MAX_SLOW;
+            PREV_FAST_MODE <= '0';
 
         ELSIF RISING_EDGE(CLK) THEN
             ENABLE <= '0';
-
-            IF COUNT >= MAX_COUNT THEN
+            IF FAST_MODE /= PREV_FAST_MODE THEN
+                PREV_FAST_MODE <= FAST_MODE;
+                COUNT <= 0;
+                IF FAST_MODE = '1' THEN
+                    MAX_COUNT <= MAX_FAST;
+                ELSE
+                    MAX_COUNT <= MAX_SLOW;
+                END IF;
+            ELSIF COUNT >= MAX_COUNT THEN
                 COUNT <= 0;
                 ENABLE <= '1';
             ELSE
